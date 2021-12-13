454b57ff13c48c8257af83d1de4b0bea2cc41ad5
anacatalves <ana.catarina.24@hotmail.com>
2021-12-13 13:02:39 +0000

    organization

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,10 +3,36 @@
 function App() {
   return (
     <>
-    <header></header>
-    <section class="search-bar"></section>
-    <main class="characters"></main>
-    <article class="filter-box"></article>
+
+    <div class="body">
+
+      <header class="nav-bar">
+        <h1>Rick & Morty Wiki</h1>
+
+        <nav> {/* componente a extrair */}
+          <p></p>
+          <p></p>
+          <p></p>
+        </nav>
+
+      </header>
+
+      <main class="container">
+
+        <div class="main-column1">
+          <article class="filter-box"></article>{/* componente a extrair */}
+        </div>
+
+        <div class="main-column2">
+          <main class="search-bar"></main>
+
+          <main class="characters"></main>{/* componente a extrair */}
+
+        </div>
+        
+      </main>
+
+    </div>
 
     </>
   );
