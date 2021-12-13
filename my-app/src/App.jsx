5ec5796e765ef348d9c70788947d64b1f3b40954
anacatalves <ana.catarina.24@hotmail.com>
2021-12-13 17:04:00 +0000

    print characters

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,42 +1,75 @@
-import Characters from "./components/characters";
+import react, { useState } from 'react';
+//import Characters from "./components/Characters";
 import './assets/css/main.css';
 
 function App() {
+  let [results, setResults] = useState([]);
+
+  let api = "https://rickandmortyapi.com/api/character";
+
+  fetch (api)
+    .then ((response) => response.json())
+    .then ((data) => setResults(data.results));
+
   return (
     <>
 
-    <div class="body">
+      <div class="body">
 
-      <header class="nav-bar">
-        <h1>Rick & Morty Wiki</h1>
+        <header class="nav-bar">
+          <h1>Rick & Morty Wiki</h1>
 
-        <nav> {/* componente a extrair */}
-          <a href="" target="_blank">Characters</a>
-          <a href="" target="_blank">Episodes</a>
-          <a href="" target="_blank">Locations</a>
-        </nav>
+          <nav> {/* componente a extrair */}
+            <a href="" target="_blank">Characters</a>
+            <a href="" target="_blank">Episodes</a>
+            <a href="" target="_blank">Locations</a>
+          </nav>
 
-      </header>
+        </header>
 
-      <main class="container">
+        <main class="container">
 
-        <div class="main-column1">
-          <article class="filter-box"></article>{/* componente a extrair */}
-        </div>
-
-        <div class="main-column2">
-          <div class="search-bar">
-            
+          <div class="main-column1">
+            {/* componente a extrair */}
+            <article class="filter-box">
+              filter-box
+            </article>
           </div>
 
-          {/* <div class="characters"></div>componente a extrair */}
-          <Characters />
+          {/* componente a extrair */}
+          <div class="main-column2">
+            <div class="search-bar">
+              
+            </div>
+          </div>
 
-        </div>
-        
-      </main>
+          
+            {results.map ((character) => (
+                <>
+                <div className='character_card'>
+                    <div className='character_image'>
+                        <img src="" alt="" />
+                    </div>
 
-    </div>
+                    <div className='character_info'>
+                        <h3>{character.name}</h3>
+                        <p>Location</p>
+                    </div>
+                    
+                    <div className='character_status'>
+                        <p>Alive</p>
+                    </div> 
+                </div>
+                </>
+            ))}
+           
+
+            {/* <Characters /> */}
+          
+          
+        </main>
+
+      </div>
 
     </>
   );
