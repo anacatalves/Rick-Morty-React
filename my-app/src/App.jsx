03c5cd94fabe9e1745b63bff892562c9ac9df2c6
anacatalves <ana.catarina.24@hotmail.com>
2021-12-14 15:31:23 +0000

    grid+search bar working!!!!

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,14 +1,30 @@
-import react from 'react';
+import {react, useState, useEffect} from 'react';
 import Characters from "./components/characters";
 import './assets/css/main.css';
 
 
 function App() {
+
+  let [results, setResults] = useState([]);
+  let [search, setSearch] = useState("");
+  let api = `https://rickandmortyapi.com/api/character/?name=${search}`;
   
+
+  useEffect(() => {
+      (async () => {
+          let response = await fetch (api);
+          let data = await response.json();
+          
+          setResults(data.results);
+          
+      })();
+  }, [api]); 
+
+
   return (
     <>
 
-      <div className="body">
+      <div className="body-container">
 
         <header className="nav-bar">
           <h1>Rick & Morty Wiki</h1>
@@ -21,45 +37,46 @@ function App() {
 
         </header>
 
-        <main className="container">
+        <main>
 
-          <div class="main-column1">
-            <h2>Filters</h2>
-            <p>Clear Filters</p>
-            {/* componente a extrair */}
-            <article class="filter-box">
-              <details>
-                <summary>Status</summary>
-                  <button className='Filter'>ALIVE </button>
-                  <button className='Filter'>DEAD </button>
-                  <button className='Filter'>UNKNOWN </button>
+          <h1 class="page-title">Characters</h1>
+
+          <div class="main-content">
+
+            <section class="main-column1">
+
+              <h2>Filters</h2>
+              <p>Clear Filters</p>
+              
+              <article class="filter-box">{/* componente a extrair */}
+                <details>
+                  <summary>Status</summary>
+                    <button className='Filter'>ALIVE </button>
+                    <button className='Filter'>DEAD </button>
+                    <button className='Filter'>UNKNOWN </button>
+                  </details>
+                <details>
+                  <summary>Species</summary>
+                    <button className='Filter'>HUMAN </button>
+                    <button className='Filter'>ALIEN </button>
+                    <button className='Filter'>MYTHOLOGICAL CREATURE</button>
                 </details>
-              <details>
-                <summary>Species</summary>
-                  <button className='Filter'>HUMAN </button>
-                  <button className='Filter'>ALIEN </button>
-                  <button className='Filter'>MYTHOLOGICAL CREATURE</button>
-              </details>
-              <details>
-                <summary>Gender</summary>
-                  <button className='Filter'>MALE </button>   
-                  <button className='Filter'>FEMALE </button>
-                  <button className='Filter'>UNKNOWN</button>
+                <details>
+                  <summary>Gender</summary>
+                    <button className='Filter'>MALE </button>   
+                    <button className='Filter'>FEMALE </button>
+                    <button className='Filter'>UNKNOWN</button>
+                </details>
+              </article>
 
-              </details>
-            </article>
+            </section>
+            
+            <section className="main-column2">
+              <Characters results={results} setSearch={setSearch} /> 
+            </section>
+
           </div>
-
-          {/* componente a extrair */}
-          <div className="main-column2">
-
-            <h1>Characters</h1>
-
-            <Characters />
-         
-          </div>
-
-    
+        
         </main>
 
       </div>
--- my-app/src/components/characters.jsx
+++ my-app/src/components/characters.jsx
@@ -1,60 +1,38 @@
-import { React, useEffect, useState } from 'react';
+import { React} from 'react';
 //import searchIcon from "../img/search-icon.png";
 
-const Characters = () => {
-
-    let [results, setResults] = useState([]);
-    let [api, setApi] = useState("https://rickandmortyapi.com/api/character");
-    let [search, setSearch] = useState(undefined);
-
-    useEffect(() => {
-        (async () => {
-            let response = await fetch (api);
-            let data = await response.json();
-            
-            setResults(data.results);
-            
-        })();
-    }, [api]); 
-
-    useEffect(() => {
-        if (search !== undefined) {
-            setApi(api = `https://rickandmortyapi.com/api/character/?name=${search}`)
-        }
-        
-    }, [search])
-
-   /*  function searchCharacter (event) {
-        setSearch(event.target.value)
-    }; */
+const Characters = ({results, setSearch}) => {
 
     return (
         <>  
             <div className="search-character">
-              <input className="search-bar" /* onClick={searchCharacter()}  */type="text" placeholder="Search for a character" value="" />
+              <input className="search-bar" onChange={ (event) => {setSearch(event.target.value); console.log(event)} } type="text" placeholder="Search for a character" />
+        
               {/* <img src={searchIcon} alt="" /> */}
             </div> 
 
-            {results.map ((character) => (
-              <>
-              <div className='character_card'>
-                  <div className='character_image'>
-                      <img className='character_image' src={character.image} alt="" />
-                  </div>
+            <div className="character-list">
+                {results.map ((character, index) => (
+                <>
+                    <div className='character_card' key={index}>
+                        <div className='character_image'>
+                            <img className='character_image' src={character.image} alt="" />
+                        </div>
 
-                  <div className='character_info'>
-                      <h3>{character.name}</h3>
-                      <p>{character.location.name}</p>
-                  </div>
-                  
-                  <div className='character_status'>
-                      <p>{character.status}</p>
-                  </div> 
+                        <div className='character_info'>
+                            <h3>{character.name}</h3>
+                            <p>{character.location.name}</p>
+                        </div>
+                        
+                        <div className='character_status'>
+                            <p>{character.status}</p>
+                        </div> 
 
-                  <button className='AddtoCart'>Add to cart</button>
-              </div>
-              </>
-          ))} 
+                        <button className='AddtoCart'>Add to cart</button>
+                    </div>
+                    </>
+                ))} 
+            </div>
         </>
     )
 }
