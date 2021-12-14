74edfa95e8edf36242cfbc0c3ddf55ee2de046d3
rmanilsv <rmanilsv@gmail.com>
2021-12-14 12:17:29 +0100

    segundo dia

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,4 +1,4 @@
-import react, { useState } from 'react';
+import react, { useState, useEffect } from 'react';
 //import Characters from "./components/Characters";
 import './assets/css/main.css';
 
@@ -7,10 +7,22 @@ function App() {
 
   let api = "https://rickandmortyapi.com/api/character";
 
-  fetch (api)
+  useEffect(() => {
+    (async () => {
+      let response = await fetch (api);
+      let data = await response.json();
+      
+      setResults(data.results);
+
+    })()
+    
+  }, [api])
+
+  const [filterParam, setFilterParam] = useState(["All"]);
+  /* fetch (api)
     .then ((response) => response.json())
     .then ((data) => setResults(data.results));
-
+ */
   return (
     <>
 
@@ -30,9 +42,29 @@ function App() {
         <main class="container">
 
           <div class="main-column1">
+            <h2>Filters</h2>
+            <p>Clear Filters</p>
             {/* componente a extrair */}
             <article class="filter-box">
-              filter-box
+              <details>
+                <summary>Status</summary>
+                  <button className='Filter'>ALIVE </button>
+                  <button className='Filter'>DEAD </button>
+                  <button className='Filter'>UNKNOWN </button>
+                </details>
+              <details>
+                <summary>Species</summary>
+                  <button className='Filter'>HUMAN </button>
+                  <button className='Filter'>ALIEN </button>
+                  <button className='Filter'>MYTHOLOGICAL CREATURE</button>
+              </details>
+              <details>
+                <summary>Gender</summary>
+                  <button className='Filter'>MALE </button>   
+                  <button className='Filter'>FEMALE </button>
+                  <button className='Filter'>UNKNOWN</button>
+
+              </details>
             </article>
           </div>
 
@@ -59,6 +91,9 @@ function App() {
                     <div className='character_status'>
                         <p>{character.status}</p>
                     </div> 
+
+                    <button className='AddtoCart'>Add to cart</button>
+
                 </div>
                 </>
             ))}
