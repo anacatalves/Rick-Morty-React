b120326901ddc242e2d5399223fba7c530a259d6
rmanilsv <rmanilsv@gmail.com>
2021-12-20 10:16:18 +0100

    episodes test

--- my-app/src/pages/Episodes.jsx
+++ my-app/src/pages/Episodes.jsx
@@ -1,10 +1,10 @@
 import {react, useState, useEffect} from 'react';
+import Characters from "../components/characters";
 
-
-function Episodes (){
+function Episodes ({results}){
     let [resultsEpisodes, setResultsEpisodes] = useState([]);
     let apiEpisodes = `https://rickandmortyapi.com/api/episode`;
-    
+
     useEffect(() => {
     async function GetEpisodes (){
         let response = await fetch (apiEpisodes); 
@@ -15,13 +15,31 @@ function Episodes (){
     });
 
     return (
-        <select>
-            {resultsEpisodes.map(({ label, value }) => (
-                <option key={value} value={value}>
-                    Episode - {label}
-                </option>
-            ))}
-        </select>
+
+        <main>
+        <h1 className="page-title">Episode: <b>Pilot</b></h1>
+
+            <div className="main-content">
+
+                <section className="main-column1">
+                    <h1>Pick Episode</h1>
+                    <article className='filter-box'>
+                        <select>
+                            {resultsEpisodes.map(({ label, value }) => (
+                                <option key={value} value={value}>
+                                Episode - {label}
+                                </option>
+                        ))}
+                        </select>
+                    </article>
+                </section>
+
+                <section className="main-column2">
+                     <Characters results={results} />  
+                </section>
+            </div>
+        </main>
+
     )
 }
 
