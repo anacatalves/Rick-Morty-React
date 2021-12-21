c4e947e55064f32b116653d2eb4abf64b6b6baf6
anacatalves <ana.catarina.24@hotmail.com>
2021-12-21 14:14:38 +0000

    shopping-cart

--- my-app/src/components/addToCart.jsx
+++ my-app/src/components/addToCart.jsx
@@ -5,9 +5,9 @@ const AddToCart = () => {
     let [cartItem, setCartItem] = useState([]);
     let [subtotal, setSubtotal] = useState("");
 
-    if () {
+    /* if () {
 
-    }
+    } */
     
     return (
         <button className='AddtoCart'>Add to cart</button>
